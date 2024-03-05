a87f768177add99c3640591b8d998270efc2232c
Martin0405 <160177898+Martin0405@users.noreply.github.com>
2024-03-05 15:36:15 -0600

    2
    2

--- color/sketch.js
+++ color/sketch.js
@@ -6,18 +6,37 @@
 // - describe what you did to take this project "above and beyond"
 
 let rectheight = 40;
+let x = 0;
+let a = 0;
+let b = 0;
+let c = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
-
+  fbackground();
+} 
 function draw() {
-fbackground();
+  
 }
-function fbackground(){
-  let y = 0;
-  while (y < height){
-    fill(map(y,0,height,0,225))
-rect(0,y,width,rectheight)
-y+=rectheight;
+function fbackground() {
+  let y = 0; 
+
+  while (y <= height) {
+
+  
+    while (x<=width) {
+      //if (mouseIsPressed)
+      //{
+ a = random(0,255);
+ b = random(0,255);
+ c = random(0,255);
+//} 
+       fill(a,b,c)
+      rect(x, y, rectheight, rectheight)
+      x += rectheight;
+    }
+    x=0
+    y += rectheight;
+  
   }
 }
