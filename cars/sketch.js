29173743d5371e9595b11268ff375bfa43952998
Martin0405 <160177898+Martin0405@users.noreply.github.com>
2024-05-17 15:26:38 -0600

    puzzle

--- cars/sketch.js
+++ cars/sketch.js
@@ -1,72 +1,165 @@
-// Objects in Objects
-// Mr. Scott
+// CAR CAR CAR
+// Martin
 // April 12, 2024
 // Storing objects IN objects, overwriting objects, basic transformations
 
 let eastbound = [];
-let wastbound = [];
-
+let westbound = [];
+let light;
+// create cars and east and west
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  for (let i = 0; i < 100; i++) {
+  light = new TrafficLight(windowWidth / 2, windowHeight / 2, 100)
+  for (let i = 0; i < 10; i++) {
     eastbound.push(new Car(0, random(0, (windowHeight / 2) - 30), 0))
 
 
   }
-  for (let i = 0; i < 100; i++) {
-    eastbound.push(new Car(windowWidth, random((windowHeight / 2)+30, windowHeight -30), 1))
+  for (let i = 0; i < 10; i++) {
+    westbound.push(new Car(windowWidth, random((windowHeight / 2) + 30, windowHeight - 30), 1))
 
 
   }
 }
-
+// show the car and road
 function draw() {
   background(70);
   road()
+
   for (let c of eastbound) {
     c.display();
-    c.move()
+    c.action()
+
+  }
+  for (let c of westbound) {
+    c.display();
+    c.action()
+
+  }
+  //show the light
+  light.display();
+}
+// create new cars 
+function mousePressed() {
+  eastbound.push(new Car(0, random(0, (windowHeight / 2) - 30), 0))
+  if (keyIsPressed && keyCode === SHIFT) {
+    westbound.push(new Car(windowWidth, random((windowHeight / 2) + 30, windowHeight - 30), 1))
 
   }
 }
-
+// create road
 function road() {
   for (let i = 0; i < windowWidth; i += 50) {
     fill(10000)
     square(i, height / 2, 20)
   }
 }
+// function for car
 class Car {
   constructor(x, y, d) {
-    this.x = x; this.y = y; 
+    this.x = x; this.y = y;
     this.speed = random(2, 10);
     this.c = color(random(255), random(255), random(255));
     this.size = 60;
     this.direction = d//0--> right//1 <-- left
   }
+  // move for car
   move() {
+    if (light.red === true) {//keep the speed when the light become green
+      this.speed = 0
+    }
+    else {
+      this.speed = constrain(this.speed, 1, 30);
 
+    }
     if (this.direction === 0) {
       this.x += this.speed
       if (this.x > windowWidth) {
         this.x = 0
       }
     }
-  else{
-    this.x -= this.speed
-    if (this.x < 0) {
-      this.x = windowWidth
+    else {
+      this.x -= this.speed
+      if (this.x < 0) {
+        this.x = windowWidth
+      }
+
+    }
+  }
+  // speed increase for car
+  speedup() {
+    let chance = random(0, 100);
+    if (chance <= 1) {
+      if (this.speed <= 30) {
+        this.speed += this.speed + (this.speed / 100);
+      }
     }
 
   }
+  // speed decrease for car
+  speeddown() {
+    let chance = random(0, 100);
+    if (chance <= 1) {
+      if (this.speed >= 10) {
+        this.speed -= this.speed - (this.speed / 100);
+      }
+    }
+
+  }
+  // color change for car
+  colorchange() {
+    let chance = random(0, 100);
+    if (chance <= 1) {
+      this.c = color(random(255), random(255), random(255));
+    }
+
+  }
+  // give these to action
+  action() {
+    this.move()
+    this.speeddown()
+    this.speedup()
+    this.colorchange()
   }
 
+ 
 
-  // Class Methods
+  // set up of car
   display() {
     fill(this.c);
-    rect(this.x, this.y, this.size, this.size * 0.5);
+    rect(this.x, this.y, this.size, this.size * 0.5)
+    fill(10000)
+    rect(this.x, this.y, 10, 10 * 0.5);
+    rect(this.x + this.size - 10, this.y, 10, 10 * 0.5)
+    rect(this.x, this.y + this.size * 0.5 - 5, 10, 10 * 0.5)
+    rect(this.x + this.size - 10, this.y + this.size * 0.5 - 5, 10, 10 * 0.5)
+  }
+}
+// make light
+class TrafficLight {
+  constructor(x, y, d) {
+    this.fram = 120;
+    this.red = false;
+  }
+  // show red when we press and remain 2 second
+  display() {
+    if (keyIsPressed && keyCode === 32) { this.red = true }
+    if (this.red === true) {
+      fill("red")
+      this.fram -= 1
+    }
+    // turn back to green 
+    if (this.fram === 0) {
+      this.red = false
+      this.fram = 120
+    }
+    if (this.red === false) {
+      fill("green")
+    }
+
+    // set up the light
+    circle(windowWidth / 2, windowHeight / 2, 100)
+
 
   }
 }
--- puzzle/sketch.js
+++ puzzle/sketch.js
@@ -9,13 +9,14 @@ let grid =
   [255, 255, 0, 255, 255]
   ];
 
-let squareSize = 50;
+let squareSize = 100;
 const NUM_ROWS = 4; const NUM_COLS = 5;
 
 let row, col;
 
 function setup() {
   createCanvas(NUM_COLS * squareSize, NUM_ROWS * squareSize);
+  randomGrid()
 }
 
 function draw() {
@@ -23,25 +24,35 @@ function draw() {
   row = getCurrentY(); print(col, row);
   background(220);
   drawGrid();
+  win()
 }
 
 function mousePressed() {
-  flip(col, row);
-  if (col + 1 <= 4) {
+  
 
-    flip(col + 1, row);
+  
+  if(keyIsPressed&&keyCode===SHIFT){
+    flip(col, row)
   }
-  if (col - 1 >= 0) {
+else{
+flip(col, row);
+if (col + 1 <= 4) {
 
-    flip(col - 1, row);
-  }
-  if (row + 1 <= 3) {
+  flip(col + 1, row);
+}
+if (col - 1 >= 0) {
 
-    flip(col, row + 1);
-  }
-  if (row - 1 >= 0) { 
+  flip(col - 1, row);
+}
+if (row + 1 <= 3) {
 
-    flip(col, row - 1); }
+  flip(col, row + 1);
+}
+if (row - 1 >= 0) {
+
+  flip(col, row - 1);
+}
+}
 }
 
 function flip(x, y) {
@@ -74,4 +85,29 @@ function drawGrid() {
       square(x * squareSize, y * squareSize, squareSize);
     }
   }
+}
+function randomGrid(){
+  for (let y = 0; y < NUM_ROWS; y++) {
+    for (let x = 0; x < NUM_COLS; x++) {
+  let r=int(random(0,2))
+  if(r===0){
+    grid[y][x]=255
+  }
+  else
+  grid[y][x]=0
+    }}
+}
+function win(){
+  let score=0
+  for (let y = 0; y < NUM_ROWS; y++) {
+    for (let x = 0; x < NUM_COLS; x++) {
+      
+if(grid[y][x]===255){score+=1}
+  if(grid[y][x]===0){score-=1}
+  if(score===20){console.log("win")
+  }
+  if(score===-20){console.log("win")
+  }
+}
+    }
 }
