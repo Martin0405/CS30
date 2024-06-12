6c1b34992946623b677f0f3c32a385232b401333
Martin0405 <160177898+Martin0405@users.noreply.github.com>
2024-06-12 15:29:24 -0600

    day14
    rotate the text, end the game while teach black and finish line

--- own/sketch.js
+++ own/sketch.js
@@ -97,24 +97,25 @@ function finishedline(){
 
 let easycam;
 let playerX, playerY;
-let x; 
+let x;
 let y;
 let timer = 200;
 let direction = 0;
 let grid =
-[[255, 0, 255, 0, 255],
-[0, 255, 0, 255, 0]
-];
-let mazeendtime=10
-let speed =5
-let squareSize=20
+  [[254, 1, 254, 1, 254],
+  [1, 254, 1, 254, 1]
+  ];
+let gameactive = true;
+let mazeendtime = 10
+let speed = 5
+let squareSize = 20
 const NUM_ROWS = 2; const NUM_COLS = 5;
 
 let row, col;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   setAttributes('antialias', true);
- x= 0;
+  x = 0;
   y = 0;
   easycam = createEasyCam();
   angleMode(DEGREES)
@@ -127,7 +128,6 @@ function setup() {
 
 let tiles = []
 function draw() {
-  //finishedline()
   noStroke();
   //Level
   background(0);
@@ -135,103 +135,123 @@ function draw() {
   mazedraw()
   finishedline();
   mazecharacter()
-  print(get(windowWidth/2,windowHeight/2))
+  print(get(windowWidth / 2, windowHeight / 2))
 
 
- 
+
 
   //Camera
-
-    easycam.panX(sqrt((5*5)/2));
-    if(mouseIsPressed){
-    easycam.panY(sqrt((5*5)/2));
+  if (gameactive === true) {
+    easycam.panX(sqrt((5 * 5) / 2));
+    if (mouseIsPressed) {
+      easycam.panY(sqrt((5 * 5) / 2));
     }
     else
-    easycam.panY(-sqrt((5*5)/2));
-
+      easycam.panY(-sqrt((5 * 5) / 2));
+  }
 }
 
-function mazecreater(){
-tiles.push([x, y])
-  while (timer >= 0) {  
-    x += sqrt((100*100)/2);
+function mazecreater() {
+  tiles.push([x, y])
+  x += sqrt((100 * 100) / 2);
+  y -= sqrt((100 * 100) / 2);
+  tiles.push([x, y])
+
+  while (timer >= 0) {
+    x += sqrt((100 * 100) / 2);
     direction = int(random(0, 2))
     if (direction === 0) {
-      y += sqrt((100*100)/2);
+      y += sqrt((100 * 100) / 2);
     }
     if (direction === 1) {
-      y -= sqrt((100*100)/2);
+      y -= sqrt((100 * 100) / 2);
     }
     tiles.push([x, y])
 
     timer--;
   }
-} 
-function mazedraw(){
-for(let t of tiles){
-  push();
-  translate(t[0],t[1])
-  rotate(45)
-  square(0,0,100)
-  pop();
 }
+function mazedraw() {
+  for (let t of tiles) {
+    push();
+    translate(t[0], t[1])
+    rotate(45)
+    square(0, 0, 100)
+    pop();
+  }
 }
 function preload() {
   font = loadFont('assets/bahnschrift.ttf');
 }
 
-function finishedline(){
+function finishedline() {
   push();
-  translate(tiles[201][0], tiles[201][1])
-  if (direction===1){
+  translate(tiles[202][0], tiles[202][1])
+  if (direction === 1) {
     rotate(45)
-     for (let row = 0; row < NUM_ROWS; row++) {
-    for (let col = 0; col < NUM_COLS; col++) {
-      fill(grid[row][col]);
-      square(0+col* squareSize,0+row * squareSize,squareSize);
-  }}}
-  else {rotate(-45)
-  
-  for (let row = 0; row < NUM_ROWS; row++) {
-    for (let col = 0; col < NUM_COLS; col++) {
-      fill(grid[row][col]);
-      square(-100+col* squareSize,100+row * squareSize,squareSize);
-  
-    }
-  }}pop();}
-  function mazecharacter(){
-   push();
-    rotate(45);
-    fill(0,0,255)
-    square(playerX,playerY) 
-      if (get(windowWidth/2,windowHeight/2)[0]===0){
-    textFont(font);
-    textSize(24);
-      fill(255,0,0)
-      text('game over', playerX,playerY);
-   }
-  if(mouseIsPressed){
-    playerX+=speed
-   }
-   else playerY-=speed
-   square(playerX,playerY,10)
-
-
-   pop();
-    }
-   /* if ( square(playerX,playerY,10)==="BLUE")
-    {
-      easycam.panX(sqrt((5*5)/2));
-      if(mouseIsPressed){
-      easycam.panY(sqrt((5*5)/2));
+    for (let row = 0; row < NUM_ROWS; row++) {
+      for (let col = 0; col < NUM_COLS; col++) {
+        fill(grid[row][col]);
+        square(0 + col * squareSize, 0 + row * squareSize, squareSize);
       }
-      else
-      easycam.panY(-sqrt((5*5)/2));
-      if(mouseIsPressed){
-        playerX+=speed
-       }
-       else playerY-=speed
-       square(playerX,playerY,10)
-    
     }
-    else */
+  }
+  else {
+    rotate(-45)
+
+    for (let row = 0; row < NUM_ROWS; row++) {
+      for (let col = 0; col < NUM_COLS; col++) {
+        fill(grid[row][col]);
+        square(-100 + col * squareSize, 100 + row * squareSize, squareSize);
+
+      }
+    }
+  } pop();
+}
+function mazecharacter() {
+  push();
+  rotate(45);
+  fill(0, 0, 255)
+  square(playerX, playerY)
+  if (get(windowWidth / 2, windowHeight / 2)[0] === 1 || get(windowWidth / 2, windowHeight / 2)[0] === 254 || get(windowWidth / 2, windowHeight / 2)[0] === 0) {
+    speed = 0   
+     textFont(font);
+    textSize(24);
+    fill(255, 0, 0)
+    push();
+    translate(playerX, playerY)
+    rotate(-45)
+    text('game over', 0,0);
+    pop();
+    gameactive = false;
+  }
+  if (mouseIsPressed) {
+    playerX += speed
+  }
+  else playerY -= speed
+
+
+  square(playerX, playerY, 10)
+  //if (playerX===tiles[201][0]&&playerY=== tiles[201][1]){
+
+  //}
+
+  pop();
+}
+
+/* if ( get(playerX,playerY)==="BLUE")
+ {
+   easycam.panX(sqrt((5*5)/2));
+   if(mouseIsPressed){
+   easycam.panY(sqrt((5*5)/2));
+   }
+   else
+   easycam.panY(-sqrt((5*5)/2));
+   if(mouseIsPressed){
+     playerX+=speed
+    }
+    else playerY-=speed
+    square(playerX,playerY,10)
+ 
+ }
+ else */
